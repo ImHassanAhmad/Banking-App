a0ee615a6d24f9a7739378e0e78a64ee3cdadf8f
Muhammad Tayyab <mt29@witty.tech>
2023-12-19 11:31:50 +0500

    implment login Api

--- src/store/api/mock/handlers/onboarding.mock.ts
+++ src/store/api/mock/handlers/onboarding.mock.ts
@@ -18,35 +18,39 @@ export const handlers = [
       constants.onBoardingConstants.DICTIONARY_COUNTRY_OF_INCORPORATION_RESPONSE
     );
   }),
-  http.post('*/v1/sme/onboarding/authentication/login', ({ request }: any): HttpResponse => {
-    const { email, password, captchaToken }: LoginRequest = request.body;
-    console.log(email, password);
-    if (
-      email === constants.onBoardingConstants.MOCK_LOGIN_EMAIL &&
-      password === constants.onBoardingConstants.MOCK_LOGIN_PASSWORD &&
-      captchaToken === constants.onBoardingConstants.MOCK_CAPTCHA_VALUE
-    )
-      return HttpResponse.json(constants.onBoardingConstants.LOGIN_RESPONSE, {
-        status: 200
-      });
+  http.post(
+    '*/v1/sme/onboarding/authentication/login',
+    async ({ request }: any): Promise<HttpResponse> => {
+      const requestData = await request.json();
+      console.log(requestData);
+      const { email, password }: LoginRequest = requestData;
+      console.log(email, password);
+      if (
+        email === constants.onBoardingConstants.MOCK_LOGIN_EMAIL &&
+        password === constants.onBoardingConstants.MOCK_LOGIN_PASSWORD
+      )
+        return HttpResponse.json(constants.onBoardingConstants.LOGIN_RESPONSE, {
+          status: 200
+        });
 
-    INCORRECT_LOGINS_COUNT += 1;
+      INCORRECT_LOGINS_COUNT += 1;
 
-    if (INCORRECT_LOGINS_COUNT > 5 && INCORRECT_LOGINS_COUNT < 10)
-      return HttpResponse.json(
-        constants.onBoardingConstants.TOO_MANY_INVALID_LOGIN_ATTEMPTS_RESPONSE,
-        {
-          status: 500
-        }
-      );
+      if (INCORRECT_LOGINS_COUNT > 5 && INCORRECT_LOGINS_COUNT < 10)
+        return HttpResponse.json(
+          constants.onBoardingConstants.TOO_MANY_INVALID_LOGIN_ATTEMPTS_RESPONSE,
+          {
+            status: 500
+          }
+        );
 
-    if (INCORRECT_LOGINS_COUNT > 10)
-      return HttpResponse.json(constants.onBoardingConstants.SYSTEM_ERROR_RESPONSE, {
+      if (INCORRECT_LOGINS_COUNT > 10)
+        return HttpResponse.json(constants.onBoardingConstants.SYSTEM_ERROR_RESPONSE, {
+          status: 400
+        });
+
+      return HttpResponse.json(constants.onBoardingConstants.INCORRECT_LOGIN_DATA_RESPONSE, {
         status: 400
       });
-
-    return HttpResponse.json(constants.onBoardingConstants.INCORRECT_LOGIN_DATA_RESPONSE, {
-      status: 400
-    });
-  })
+    }
+  )
 ];
