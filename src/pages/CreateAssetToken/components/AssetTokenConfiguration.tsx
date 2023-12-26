783688e7449e3c4f0c2e68b5e5812fed4e9573d0
Muhammad Mubeen <mm47@witty.tech>
2023-12-26 14:06:01 +0500

    asset config list enum used

--- src/pages/CreateAssetToken/components/AssetTokenConfiguration.tsx
+++ src/pages/CreateAssetToken/components/AssetTokenConfiguration.tsx
@@ -4,32 +4,34 @@ import { useTranslation } from 'react-i18next';
 import CheckboxItem from '@app/components/CheckboxItem';
 import { type CheckListType } from '@app/components/CheckboxItem/types';
 import { RouteNames } from '@app/constants/routes';
+import { AssetConfigKeys } from '../types';
 
 const assetTokenNamespace = RouteNames.CREATE_ASSET_TOKEN;
 
 const AssetTokenConfigurations: React.FC = () => {
   const { t } = useTranslation();
 
-  const CHECK_LIST: CheckListType = {
-    token_config_pausable: { checked: true, link: '#' },
-    token_config_mint: { checked: true, link: '#' },
-    token_config_burn: { checked: false, link: '#', allowAction: true },
-    token_config_capped: { checked: false, link: '#', allowAction: true }
+  const CHECK_LIST: Record<
+    AssetConfigKeys,
+    { checked: boolean; link: string; allowAction?: boolean }
+  > = {
+    [AssetConfigKeys.TokenConfigPausable]: { checked: true, link: '#' },
+    [AssetConfigKeys.TokenConfigMint]: { checked: true, link: '#' },
+    [AssetConfigKeys.TokenConfigBurn]: { checked: false, link: '#', allowAction: true },
+    [AssetConfigKeys.TokenConfigCapped]: { checked: false, link: '#', allowAction: true }
   };
 
   const [checkList, setCheckList] = useState<CheckListType>(CHECK_LIST);
 
   const handleCheckboxChange = useCallback(
     (key: string): void => {
-      if (checkList[key].allowAction) {
-        setCheckList((prevCheckList) => ({
-          ...prevCheckList,
-          [key]: {
-            ...prevCheckList[key],
-            checked: !prevCheckList[key].checked
-          }
-        }));
-      }
+      setCheckList((prevCheckList) => ({
+        ...prevCheckList,
+        [key]: {
+          ...prevCheckList[key],
+          checked: !prevCheckList[key].checked
+        }
+      }));
     },
     [checkList]
   );
--- src/pages/CreateAssetToken/types.ts
+++ src/pages/CreateAssetToken/types.ts
@@ -5,3 +5,17 @@ export enum CreateAssetTokenFlowSteps {
   tokenConfigutration = 'Token Configuration',
   tokenPrice = 'Token Price'
 }
+
+export enum AssetConfigList {
+  pausable = 'token_config_pausable',
+  mint = 'token_config_mint',
+  burn = 'token_config_burn',
+  capped = 'token_config_capped'
+}
+
+export enum AssetConfigKeys {
+  TokenConfigPausable = 'token_config_pausable',
+  TokenConfigMint = 'token_config_mint',
+  TokenConfigBurn = 'token_config_burn',
+  TokenConfigCapped = 'token_config_capped'
+}
