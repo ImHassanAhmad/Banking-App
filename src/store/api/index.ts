7e60aab585a4101c5f3930ea6da4a44a0a38116f
Zain Ul Abedin <za61@PKMACZA61W1TTY.local>
2024-01-02 17:41:51 +0500

    fix: conflict issue resolved

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -1,5 +1,6 @@
 import { onBoardingApi } from './onboarding';
 import { loginApi } from './login';
 import { assetTokenApi } from './tokens';
+import { assetApi } from './asset';
 
-export const apiEndpoints = [onBoardingApi, loginApi, assetTokenApi];
+export const apiEndpoints = [onBoardingApi, loginApi, assetApi, assetTokenApi];
