bf1b39881595cc3656aabe78234b7dfdf5f891df
Muhammmad Shaheryar <ms19@witty.tech>
2024-01-08 10:32:05 +0500

    FEW-27: testcase for TaxReporter step

--- src/pages/TaxReporter/TaxReporter.test.tsx
+++ src/pages/TaxReporter/TaxReporter.test.tsx
@@ -1,23 +1,58 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import TaxReporter from './TaxReporter';
-import { RouteNames } from '@app/constants/routes';
-import { en } from '@app/i18n/translations';
-import transformation from '@app/utils/LanguageTransformation';
-const mockRoutes = RouteNames;
-const mockLanguage = en;
-const mockTransformation = transformation(mockLanguage, mockRoutes.TAX_REPORTER);
+
+// Mock the necessary dependencies for the component
 jest.mock('react-i18next', () => ({
-  useTranslation: () => ({
-    t: (key: string) => mockTransformation[key]
-  })
+  useTranslation: () => ({ t: (key: any) => key })
 }));
+
 describe('TaxReporter Component', () => {
-  it('calls the handleSave function when the continue button is clicked', () => {
-    const mockUpdateActiveStep = jest.fn();
-    const { getByText } = render(<TaxReporter updateActiveStep={mockUpdateActiveStep} />);
-    const continueButton = getByText(en[RouteNames.TAX_REPORTER].continue);
-    fireEvent.click(continueButton);
-    expect(mockUpdateActiveStep).toHaveBeenCalledTimes(1);
+  test('renders TaxReporter component with form field and error message', () => {
+    render(
+      <TaxReporter
+        updateActiveStep={() => {}}
+        registerUser={() => {}}
+        userPayload={{ peselNumber: '' }}
+      />
+    );
+
+    // Ensure form field is rendered
+    expect(screen.getByLabelText(/number_name/i)).toBeInTheDocument();
+
+    // Ensure error message is rendered
+    expect(screen.getByText(/stateserror/i)).toBeInTheDocument();
+  });
+
+  test('submits form successfully', async () => {
+    const updateActiveStepMock = jest.fn();
+    const registerUserMock = jest.fn();
+
+    render(
+      <TaxReporter
+        updateActiveStep={updateActiveStepMock}
+        registerUser={registerUserMock}
+        userPayload={{ peselNumber: '' }}
+      />
+    );
+
+    // Type into the input field
+    await userEvent.type(screen.getByLabelText(/number_name/i), '123456789');
+
+    // Submit the form
+    fireEvent.click(screen.getByText(/continue/i));
+
+    // Wait for form submission
+    await waitFor(() => {
+      // Ensure form is submitted successfully
+      expect(registerUserMock).toHaveBeenCalledWith({
+        payload: {
+          peselNumber: '123456789',
+          dryRun: true
+        },
+        onSuccess: expect.any(Function)
+      });
+    });
   });
 });
